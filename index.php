619ebf13c0745e288dd9cf90b4a72462083171a7
Tomasz Sowa <56936383+TomaszSowa1@users.noreply.github.com>
2021-04-29 22:25:19 +0200

    first draft with css

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 
-echo "yepcock";
+echo "hello world";
 
---
+++ mainpage/mainpage.php
@@ -0,0 +1,22 @@
+<?php
+echo '<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="mainpage-style.css">
+<link rel="stylesheet" href="/main-style.css">
+<script src="mainpage-js.js"></script>
+<script src="/main-js.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<div class="topnav">
+  <a class="active" href="./mainpage.php">mainpage</a>
+  <a href="/login/login.php">login</a>
+  <a href="./contact">Contact</a>
+  <a href="./about">About</a>
+</div>';
+
+echo '
+</body>
+</html>
+';    
